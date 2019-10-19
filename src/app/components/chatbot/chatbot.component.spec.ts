da0206ad81977136a4b6b2fd29293d3258e13430
rparkar <rparkar@RENTLAPT0011>
2019-10-19 12:29:50 +0530

    Created Header Module and refactoring

--- src/app/components/chatbot/chatbot.component.spec.ts
+++ src/app/components/chatbot/chatbot.component.spec.ts
@@ -1,23 +1,22 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { ChatbotComponent } from './chatbot.component';
-import { ChatHeaderComponent } from './chat-header/chat-header.component';
-import { ChatBodyComponent } from './chat-body/chat-body.component';
-import { ChatInputComponent } from './chat-input/chat-input.component';
-import { FormsModule } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ChatbotComponent } from "./chatbot.component";
+import { ChatBodyComponent } from "./chat-body/chat-body.component";
+import { ChatInputComponent } from "./chat-input/chat-input.component";
+import { FormsModule } from "@angular/forms";
+import { HttpClient } from "@angular/common/http";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { HeaderModule } from "src/app/modules/header/header.module";
 
-describe('ChatbotComponent', () => {
+describe("ChatbotComponent", () => {
   let component: ChatbotComponent;
   let fixture: ComponentFixture<ChatbotComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ChatbotComponent,ChatHeaderComponent,ChatBodyComponent,ChatInputComponent],
-      imports:[FormsModule,HttpClientTestingModule]
-    })
-    .compileComponents();
+      declarations: [ChatbotComponent, ChatBodyComponent, ChatInputComponent],
+      imports: [FormsModule, HttpClientTestingModule, HeaderModule]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -26,7 +25,7 @@ describe('ChatbotComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -1,21 +1,19 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/internal/Observable';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs/internal/Observable";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class LocationService {
   response: Response;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   private ApiURL =
-    "http://3.132.150.120:80/api/restrauntsearch?localityverbose=";
+    "http://172.16.5.195:5000/api/restrauntsearch?localityverbose=";
 
-    GetResponse(userInput: string){
-      return this.http.get(this.ApiURL+userInput);
-    }
-
-
+  GetResponse(userInput: string) {
+    return this.http.get(this.ApiURL + userInput);
+  }
 }
