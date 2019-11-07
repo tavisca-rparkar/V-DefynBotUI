f08ffbe057a7012507ed1e64f266b32a0af4896f
Neelesh Rajpurohit <nrajpurohit@tavisca.com>
2019-11-07 17:45:41 +0530

    updated app header

--- src/app/modules/header/header.component.ts
+++ src/app/modules/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { Meta } from "@angular/platform-browser";
+import { Router } from '@angular/router';
 
 @Component({
   selector: "app-header",
@@ -12,11 +13,18 @@ export class HeaderComponent implements OnInit {
   @Input() pointBalance:number;
 
 
-  constructor( private meta: Meta) {
+  constructor( private meta: Meta,
+    private _router:Router) {
     this.meta.addTag({
       name: "viewport",
       content: "width=device-width, initial-scale=1.0"
     });
   }
   ngOnInit() {}
+
+  logout(){
+    localStorage.clear();
+    this._router.navigate(['./launcher']);
+  }
+  
 }
