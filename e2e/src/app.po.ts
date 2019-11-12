5cfa1219ab8534d64255714c1c00ae8759a4b388
tavisca-rgupta-2019 <rgupta@tavisca.in>
2019-11-12 16:23:20 +0530

    added e2e for launcher

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -7,13 +7,13 @@ export class AppPage {
 
   getTitleText() {
 
-    return element(by.css('app-header div.header h1')).getText() as Promise<string>;
+    return element.all(by.css('app-root app-chatbot div app-header div div')).get(1).getText() as Promise<string>;
   }
   getTextBox(){
     return element(by.css('app-chat-input div input'));
   }
   sendText(){
-    return element(by.css('app-chat-input div button'));
+    return element.all(by.css('app-chat-input div button')).last();
   }
   getTextBubble(){
     return element(by.css('app-chat-body app-text-bubble div p')).getText() as Promise<string>;
@@ -25,24 +25,67 @@ export class AppPage {
     return element.all(by.buttonText('Order Food'));
   }
   getLatestElement(){
-    return element.all(by.css('app-chat-body app-text-bubble div p')).last();
+    return element.all(by.css('app-text-bubble')).last().element(by.css('div p')).getText();
   }
   getCorousal(){
-    return element(by.buttonText('See More Details'));
+    return element(by.className('slide-card'));
   }
   getCard(){
-    return element(by.buttonText('Proceed To Book'));
+    return element(by.className('proceed-button'));
   }
   getCourousalAgain(){
     return element(by.buttonText('Browse more restaurants'));
   }
+  getLatestCorousal()
+  {
+    return element.all(by.css('app-carousel')).last();
+  }
+  getLatestCorousalCard()
+  {
+    return this.getLatestCorousal().all(by.className('slide-card')).get(0);
+  }
+  getLatestCard()
+  {
+     return element.all(by.className('proceed-button')).last();
+  }
   getBrowserStop(){
     return element(by.buttonText('NO button present'));
   }
-  getRightArrow(){
-    return element.all(by.css('app-chat-body div div carousel div a')).last();
+  swipeRight(){
+    return  this.getLatestCorousal().element(by.className('slide'));
+   
   }
-  getLeftArrow(){
-    return element(by.css('app-chat-body div div carousel div a'));
+  getDummyButton()
+  {
+    return element(by.css('dummy-button'));
+   
   }
+  swipeLeft(){
+    return  this.getLatestCorousal().element(by.className('slide'));
+  }
+  getProceedToPayButton()
+  {
+    return element.all(by.className('proceedToPay')).last();
+  }
+  getCancelBookingButton()
+  {
+    //console.log(element(by.className('cancel')))
+    return element(by.buttonText('Cancel Booking'));
+    
+  }
+  getCheckoutSummaryCard()
+  {
+    return element(by.css('app-bookingcheckoutcard'));
+  }
+  getLauncher()
+  {
+    return element(by.css('app-launcher'));
+  }
+  getLaunchButton()
+  {
+    
+  }
+  
+
 }
+
