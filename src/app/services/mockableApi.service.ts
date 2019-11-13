490e78a77861de6ec357618273afb170a5465468
rparkar <rparkar@RENTLAPT0011>
2019-11-13 10:31:33 +0530

    Mockable.io api call made to https

--- src/app/services/mockableApi.service.ts
+++ src/app/services/mockableApi.service.ts
@@ -3,8 +3,8 @@ import { HttpClient } from "@angular/common/http";
 import { DialogflowApiService } from "./dialogflowApi.service";
 import { LocationApiService } from "./locationApi.service";
 import { RestaurantApiService } from "./restaurant-api.service";
-import { FoodOrderingService } from './food-ordering.service';
-import { LauncherService } from './launcher.service';
+import { FoodOrderingService } from "./food-ordering.service";
+import { LauncherService } from "./launcher.service";
 
 @Injectable({
   providedIn: "root"
@@ -20,7 +20,7 @@ export class MockableApiService {
   ) {}
 
   // Mockable.io URL for custom made Api for dialogflow key and api urls
-  private _apiUrl = "http://demo8483055.mockable.io/dialogflowAuthKey";
+  private _apiUrl = "https://demo8483055.mockable.io/dialogflowAuthKey";
 
   async GetResponse() {
     try {
