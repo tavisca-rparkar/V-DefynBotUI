f4e540d7c2feccd802d0c937b4297e0a6b693cd3
rparkar <rparkar@RENTLAPT0011>
2019-11-14 09:42:40 +0530

    Testing gradient for VISA client

--- src/app/clients/visa/theme.ts
+++ src/app/clients/visa/theme.ts
@@ -4,6 +4,8 @@ export const visa: Theme = {
   name: "VISA",
   style: {
     "--theme-color": "#1a1f71",
+    "--gradient-color":
+      "linear-gradient(to bottom right, #1a1f71, #434787, #8487b5);",
     icon:
       "https://lavca.org/wp-content/uploads/2019/07/VISA-logo-square-768x768.png",
     "--mic-icon-color": "#ff0000"
